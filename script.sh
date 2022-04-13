a266c6c763a9d79dd8b827ff9cb4c21cc3fcba1f
divyansh42 <diagrawa@redhat.com>
2022-04-13 16:01:53 +0530

    Test self hosted

---
+++ script.sh
@@ -0,0 +1,3 @@
+helm install $RELEASE_NAME openshift-actions-runner/actions-runner --set runnerImage=quay.io/redhat-github-actions/buildah-runner --set runnerTag=latest --set-string ephemeral=true --set-string githubPat=$GITHUB_PAT --set-string githubOwner=$GITHUB_OWNER --set-string githubRepository=$GITHUB_REPO \
+&& echo "---------------------------------------" \
+&& helm get manifest $RELEASE_NAME | kubectl get -f -
